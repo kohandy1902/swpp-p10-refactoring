857024c2a4caba52775e847dd897d6bac1eeeb62
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-16 19:16:15 +0900

    Solve P1

--- refactor.py
+++ refactor.py
@@ -1,7 +1,3 @@
-# TODO: 1. Use one-liners
-def count_asterisk_exceed_five(text):
-    return text.count("*") > 5
-
 if __name__ == "__main__":
     text = """Because he's the hero Gotham deserves but not the one it needs right now.
 So we will hunt him, because he can take it. Because he's not out hero.
@@ -17,20 +13,28 @@ He is a silent guardian, a watchful protector... a dark knight."""
     # Censor `word1` from `text`
     tmp = text
     while word1 in tmp:
-        tmp = tmp[:tmp.find(word1)] + "*" * len(word1) + tmp[tmp.find(word1) + len(word1):]
+        tmp = (
+            tmp[: tmp.find(word1)]
+            + "*" * len(word1)
+            + tmp[tmp.find(word1) + len(word1) :]
+        )
     text = tmp
 
-    if count_asterisk_exceed_five(text):
+    if text.count("*") > 5:
         print("More than five *")
 
     # TODO: 2. Extract method, remove duplicated code
     # Censor `word2` from `text`
     tmp = text
     while word2 in tmp:
-        tmp = tmp[:tmp.find(word2)] + "*" * len(word2) + tmp[tmp.find(word2) + len(word2):]
+        tmp = (
+            tmp[: tmp.find(word2)]
+            + "*" * len(word2)
+            + tmp[tmp.find(word2) + len(word2) :]
+        )
     text = tmp
 
-    if count_asterisk_exceed_five(text):
+    if text.count("*") > 5:
         print("More than five *")
 
     # Print result `text`
