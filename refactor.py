4251a2f6b1578b5069d453681cb51e9255ac5cb2
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-16 19:24:36 +0900

    Solve P3

--- refactor.py
+++ refactor.py
@@ -1,10 +1,8 @@
-def make_asterisk(text, word):
-    return text.replace(word, "*" * len(word))
-
-
-def alert_text(text):
-    if text.count("*") > 5:
-        print("More than five *")
+def censor_alert(text, *words):
+    for word in words:
+        text = text.replace(word, "*" * len(word))
+        if text.count("*") > 5:
+            print("More than five *")
 
 
 if __name__ == "__main__":
@@ -16,19 +14,7 @@ He is a silent guardian, a watchful protector... a dark knight."""
     word2 = "silent"
 
     # TODO: 4. Consider general usage
-    # TODO: 3. Substitute algorithm
-
-    # TODO: 2. Extract method, remove duplicated code
-    # Censor `word1` from `text`
-    text = make_asterisk(text, word1)
-
-    alert_text(text)
-
-    # TODO: 2. Extract method, remove duplicated code
-    # Censor `word2` from `text`
-    text = make_asterisk(text, word2)
-
-    alert_text(text)
+    censor_alert(text, word1, word2)
 
     # Print result `text`
     print(text)
